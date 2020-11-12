69908d98d10dc293e98878425e54fb9942af4cd3
nuclearbraineater <nuclearbraineater@gmail.com>
2020-11-12 10:34:25 +0100

    Added insert, update and delete logic

---
+++ BlazorServer/Models/SpiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServer.Models
+{
+    public class SpiceModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
